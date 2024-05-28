91d3bb220837f8f703ba16edc1f4b71b566ec9a8
Jimmy Hedström <jimmy.hedstrom93@gmail.com>
2024-05-28 16:44:47 +0200

    Update SES lambda

--- code/email-forward.py
+++ code/email-forward.py
@@ -5,6 +5,7 @@ from email.mime.text import MIMEText
 
 region = os.environ['Region']
 
+
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
     ses = boto3.client('ses', region)
@@ -24,14 +25,16 @@ def lambda_handler(event, context):
     email_content = obj['Body'].read().decode('utf-8')
 
     msg = email.message_from_string(email_content)
-    from_addr = msg['From']
+    from_addr = os.environ['MailSender']
     to_addr = os.environ['MailRecipient']
     subject = msg['Subject']
+    reply_to = msg['From']
 
     new_msg = MIMEText(msg.get_payload())
     new_msg['From'] = from_addr
     new_msg['To'] = to_addr
     new_msg['Subject'] = subject
+    new_msg['Reply-To'] = reply_to
 
     response = ses.send_raw_email(
         Source=from_addr,
